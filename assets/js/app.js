c7edad803972f013a99b66bb186c131a0780bc50
Ivan Seidel <ivanseidel@gmail.com>
2014-01-15 17:05:58 -0200

    Now Model and Controller classes are included in /js/app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,71 +1,95 @@
 /**
- * app.js
- *
- * This file contains some conventional defaults for working with Socket.io + Sails.
- * It is designed to get you up and running fast, but is by no means anything special.
- *
- * Feel free to change none, some, or ALL of this file to fit your needs!
+	This file contains Models, Views, Controllers and also
+	common initializations such as Socket.io
+
+	All is stored in App object, and organized as follows:
+	+       App.Models: Models Classes
+	+  App.Controllers: Controller Classes
+	+        App.Views: View Classes (Should be empty. Avoid putting view stuff here)
+	+       App.Mixins: Utility methods to mix with models/controllers
+	+            App.*: Instanced object of models, controllers, views...
  */
 
+var App = {
+	Models: {},
+	Collections: {},
+	Views: {},
+	Mixins: {},
+};
 
-(function (io) {
+/*
+	Define Models
+*/
 
-  // as soon as this file is loaded, connect automatically, 
-  var socket = io.connect();
-  if (typeof console !== 'undefined') {
-    log('Connecting to Sails.js...');
-  }
+// Team
+App.Models.Team = Backbone.Model.extend({
+	urlRoot: '/teams',
+});
 
-  socket.on('connect', function socketConnected() {
+// Team Collection
+App.Collections.Teams = Backbone.Collection.extend({
+	url: '/teams/find',
+	model: App.Models.Team
+});
 
-    // Listen for Comet messages from Sails
-    socket.on('message', function messageReceived(message) {
+// (function (io) {
 
-      ///////////////////////////////////////////////////////////
-      // Replace the following with your own custom logic
-      // to run when a new message arrives from the Sails.js
-      // server.
-      ///////////////////////////////////////////////////////////
-      log('New comet message received :: ', message);
-      //////////////////////////////////////////////////////
+//   // as soon as this file is loaded, connect automatically, 
+//   var socket = io.connect();
+//   if (typeof console !== 'undefined') {
+//     log('Connecting to Sails.js...');
+//   }
 
-    });
+//   socket.on('connect', function socketConnected() {
 
+//     // Listen for Comet messages from Sails
+//     socket.on('message', function messageReceived(message) {
 
-    ///////////////////////////////////////////////////////////
-    // Here's where you'll want to add any custom logic for
-    // when the browser establishes its socket connection to 
-    // the Sails.js server.
-    ///////////////////////////////////////////////////////////
-    log(
-        'Socket is now connected and globally accessible as `socket`.\n' + 
-        'e.g. to send a GET request to Sails, try \n' + 
-        '`socket.get("/", function (response) ' +
-        '{ console.log(response); })`'
-    );
-    ///////////////////////////////////////////////////////////
+//       ///////////////////////////////////////////////////////////
+//       // Replace the following with your own custom logic
+//       // to run when a new message arrives from the Sails.js
+//       // server.
+//       ///////////////////////////////////////////////////////////
+//       log('New comet message received :: ', message);
+//       //////////////////////////////////////////////////////
 
-
-  });
+//     });
 
 
-  // Expose connected `socket` instance globally so that it's easy
-  // to experiment with from the browser console while prototyping.
-  window.socket = socket;
+//     ///////////////////////////////////////////////////////////
+//     // Here's where you'll want to add any custom logic for
+//     // when the browser establishes its socket connection to 
+//     // the Sails.js server.
+//     ///////////////////////////////////////////////////////////
+//     log(
+//         'Socket is now connected and globally accessible as `socket`.\n' + 
+//         'e.g. to send a GET request to Sails, try \n' + 
+//         '`socket.get("/", function (response) ' +
+//         '{ console.log(response); })`'
+//     );
+//     ///////////////////////////////////////////////////////////
 
 
-  // Simple log function to keep the example simple
-  function log () {
-    if (typeof console !== 'undefined') {
-      console.log.apply(console, arguments);
-    }
-  }
+//   });
+
+
+//   // Expose connected `socket` instance globally so that it's easy
+//   // to experiment with from the browser console while prototyping.
+//   window.socket = socket;
+
+
+//   // Simple log function to keep the example simple
+//   function log () {
+//     if (typeof console !== 'undefined') {
+//       console.log.apply(console, arguments);
+//     }
+//   }
   
 
-})(
+// })(
 
-  // In case you're wrapping socket.io to prevent pollution of the global namespace,
-  // you can replace `window.io` with your own `io` here:
-  window.io
+//   // In case you're wrapping socket.io to prevent pollution of the global namespace,
+//   // you can replace `window.io` with your own `io` here:
+//   window.io
 
-);
+// );
