e5d204e25622a1e26136c9e95fc70e8cd20a034e
Ivan Seidel <ivanseidel@gmail.com>
2014-01-16 14:50:17 -0200

    Created Utility function to help with collapse. Collapsing data in Tables

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,2 +1,39 @@
 var Util = {};
 
+/*
+	Utility method to help with collapsible views.
+
+	$trigger: an jQuery element to listen to click events
+	 $target: jQuery element to collapse un-collapse when clicked
+	    opts: additional options
+*/
+Util.makeCollapsible = function($trigger, $target, opts){
+
+	opts = _.defaults(opts || {}, {
+		showData: 'Show',
+		hideData: 'Hide',
+	});
+
+	updateText();
+
+	$trigger.unbind('click').click(toggleCollapse);
+
+	function toggleCollapse(){
+		// Is it colapsed?
+		if($target.hasClass('collapse')){
+			$target.collapse('show');
+		}else{
+			$target.collapse('hide');
+		}
+		updateText();
+	}
+
+	// Change the trigger text depending if it's collapsed or not
+	function updateText(){
+		if($target.hasClass('collapse')){
+			$trigger.html(opts.showData);
+		}else{
+			$trigger.html(opts.hideData);
+		}
+	}
+}
