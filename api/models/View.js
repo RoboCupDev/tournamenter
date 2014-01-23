9e33a72493a69ef932daee1045e9e8508eab3b76
Ivan Seidel <ivanseidel@gmail.com>
2014-01-22 23:31:24 -0200

    View Model created

---
+++ api/models/View.js
@@ -0,0 +1,81 @@
+/**
+ * View
+ *
+ * @module      :: Model
+ * @description :: Represent a view (a collection of pages, with options)
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+	tableName: 'teams',
+
+	attributes: {
+
+		/*
+			The title of the page to be shown
+		*/
+		title: {
+			type: 'string',
+			defaultsTo: '[View Title]'
+		},
+
+		/*
+			Template page to be used.
+			The logic is:
+				Search for template inside '/views/view/<template>.ejs'
+		*/
+		template: {
+			type: 'string',
+			defaultsTo: 'default'
+		},
+
+		/*
+			This is the array that contains all pages to be shown
+			on the client. It's structure should follow the following:
+
+			views: [
+				{
+					module: string,
+					still: int,
+					enabled: boolean,
+					options: {},
+					data: {},
+				},
+				{...}
+			]
+			
+			MODULE:
+				The module associated to it. In the server side,
+				it's responsable for modifying data before responding
+				to requests.
+				
+				On the Client side, it's responsable for rendering
+				it's views and configurations.
+
+				It will be loaded from '/api/view_modules/<module>/server.js'
+
+			STILL:
+				still is the time (in ms) to be still on the page.
+				After this time, another page will be shown...
+
+			ENABLED:
+				If the view will be shown or not
+			
+			OPTIONS:
+				Saved options about this view. This is requrired by the module
+				only, and is not essential.
+
+			DATA:
+				A reserved place to return data. Example:
+				If your module needs to associate things with some options, and
+				return information about it, return in the 'data' hash.
+
+		*/
+		views: {
+			type: 'array',
+			defaultsTo: []
+		},
+	},
+
+};
