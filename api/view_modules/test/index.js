4a327d08ba69577fe5b5cfe156ebaab001cd8614
Ivan Seidel <ivanseidel@gmail.com>
2014-01-23 21:04:07 -0200

    Changed view module client file place. Test module changed

--- api/view_modules/test/index.js
+++ api/view_modules/test/index.js
@@ -2,12 +2,17 @@
 /*
 	Simple test view-module
 */
-exports.process = function (page, next) {
-	// Inject test attrib
-	page.test = page.still*10;
+module.exports = {
 
-	next(null, page);
-};
+	initialize: function(sails){
+		// Public js files to inject in templates
+		// exports.jsFilesToInclude = ['client.js'];
+	},
 
-// Public js files to inject in templates
-exports.jsFilesToInclude = ['client.js'];
+	process: function (page, next) {
+		// Inject test attrib
+		page.test = page.still*10;
+
+		next(null, page);
+	}
+}
