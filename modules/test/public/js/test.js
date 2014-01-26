a31c1c9a5b88207ecf9696bf23cea4c6188d131f
Ivan Seidel <ivanseidel@gmail.com>
2014-01-25 22:31:07 -0200

    Start working on default pageview module

--- modules/test/public/js/test.js
+++ modules/test/public/js/test.js
@@ -1,1 +1,33 @@
 var __ok = true;
+
+/*
+	Simple module
+*/
+var testPageModule = {
+	name: 'Test View',
+	module: 'test',
+	disabled: false,
+	// this.itemView = itemView;
+	// this.barView = barView;
+	// this.configView = configView;
+};
+
+testPageModule.itemView = Backbone.View.extend({
+
+	template: _.template($('#template-test-itemView').html()),
+
+	initialize: function(){
+		console.log('Initializing itemView');
+
+	},
+
+	render: function(){
+		console.log('Rendering itemView');
+		
+
+		return this;
+	}
+});
+
+// Register in modules
+App.PageViews.test = testPageModule;
