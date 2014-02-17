de1406ee43449821f24658f69a74843573b5b596
Ivan Seidel <ivanseidel@gmail.com>
2014-02-17 01:28:37 -0300

    Basic pageview-table implementation done.

--- modules/pageview-table/public/linker/js/pageview-table.js
+++ modules/pageview-table/public/linker/js/pageview-table.js
@@ -81,7 +81,8 @@
 		initialize: function(){
 			// Call super constructor
 			this._initialize();
-			this.listenTo(this.model, 'change:options', this.render);
+			this.listenTo(this.model, 'change:options change:data', this.render);
+			// this.listenTo(this.model, 'change:data', this.updateTables);
 		},
 
 		render: function(){
@@ -111,10 +112,113 @@
 			var finalLayout = template({
 				tables: tableViews
 			});
-			console.log(finalLayout);
 
 			this.$el.html(finalLayout);
 			this.$el.addClass('pageview-table');
+
+			this.updateTables();
+		},
+
+		updateTables: function(){
+			console.log('updateTables');
+			var tables = this.model.get('data');
+			var $tables = this.$el.find('table');
+			for(var t in tables){
+				if(!$tables[t]) return console.info('Ops... DOM Table not found');
+
+				var table = tables[t];
+				var $table = $tables[t];
+				var header = this.makeTableHeader(table);
+				var data = this.makeTableContent(table);
+
+				App.Mixins.createTable(header, data, $table);
+			}
+		},
+
+		/*
+		Table Data example
+			table:{
+				columns: "5",
+				createdAt: "2014-01-16T19:10:21.861Z",
+				evaluateMethod: "return Math.random();",
+				headerFinal: "Final",
+				headerRank: "Rank",
+				headerScore: "Round",
+				headerTeam: "Team",
+				locked: "no",
+				name: "Oficial Rounds",
+				sort: "desc",
+				updatedAt: "2014-01-30T02:41:41.302Z",
+				id: "52d82e9d672c2ccc04e0c775",
+				scores: [...]
+		*/
+		makeTableHeader: function(table){
+			var alignRight = 'text-right';
+			var headers = {
+				'rank': {
+					value: table.headerRank,
+					class: alignRight
+				},
+				'teamName': table.headerTeam,
+			};
+			// Create dynamic score fields
+			for(var k = 1; k <= table.columns*1; k++){
+				headers['score'+k] = {
+					value: table.headerScore+' '+k,
+					class: alignRight,
+				};
+			}
+
+			headers['final'] = {
+				value: table.headerFinal,
+				class: alignRight
+			};
+
+			return headers;
+		},
+
+		/*
+		Table Scores Example:
+			scores: [
+				{
+					createdAt: "2014-01-24T03:53:58.687Z",
+					scores: {
+						1: {value: 213, data: {...}}
+					},
+					tableId: "52d82e9d672c2ccc04e0c775",
+					teamId: "52d35c1e6bda8a5c8f8a22a9",
+					updatedAt: "2014-01-24T03:54:02.213Z",
+					id: "52e1e3d6dbb78102006b8397",
+					final: 0.733663140097633,
+					rank: 1,
+					team: {
+						category: "default",
+						country: "LK",
+						createdAt: "2014-01-13T03:23:10.011Z",
+						name: "XLSd",
+						updatedAt: "2014-02-07T09:52:40.193Z",
+						id: "52d35c1e6bda8a5c8f8a22a9"
+					}
+				},
+				{...}
+			]
+		*/
+		makeTableContent: function(table){
+			var content = [];
+			var columns = table.columns*1;
+
+			for(var s in table.scores){
+				var score = table.scores[s];
+				// Add dynamic attributes
+				score.teamName = score.team.name;
+				for(var c = 1; c <= columns; c++){
+					var scoreValue = (score.scores[c] ? score.scores[c].value : '-');
+					score['score'+c] = scoreValue;
+				}
+				content.push(score);
+			}
+
+			return content;
 		},
 
 	});
