51bc0f17de0d8ae9a6b13c3d41512f0fa9d2fd89
Ivan Seidel <ivan@tendadigital.net>
2014-05-26 23:34:31 -0300

    Start working with pageview-group

--- api/controllers/GroupController.js
+++ api/controllers/GroupController.js
@@ -17,7 +17,7 @@
 
  var _ = require('lodash');
 
-module.exports = {
+var GroupController = module.exports = {
 	
 	post: function (req, res, next) {
 		return XEditable.handle(Group)(req, res, next);
@@ -37,98 +37,10 @@ module.exports = {
 		var id = req.param('id');
 		if(id) options.where.id = id;
 
-
-		var finding = Group.find(options);
-
-		finding.done(function afterFound(err, models) {
-			if(!err)
-				afterFindGroups(models);
-		});
-
-		// Data to be rendered
-		var data = [];
-		// Wait for parallel tasks to complete
-		var completed = 0;
-
-		// After finishing search
-		function afterFindGroups(models){
-			data = models;
-			completed = 0;
-			
-			if(data.length <= 0)
-				return finishRendering();
-
-			// Load Matches for each Group and associates
-			data.forEach(function(group){
-
-				Match.find()
-					.where({'groupId': group.id})
-					.sort('day ASC')
-					.sort('hour ASC')
-					.sort('id')
-					.then(function(matches){
-					// Associate matches with groups
-					group.matches = matches;
-
-					// Compute scoring table for each group
-					// (must be computed AFTER associating matches with it)
-					group.table = group.table();
-
-					// Callback
-					loadedModel();
-				});
-			});
-		}
-
-		// Function needed to wait for all queries to finish
-		function loadedModel(){
-			completed++;
-
-			if(completed >= data.length)
-				associateTeams();
-		}
-
-		// Load Teams
-		function associateTeams(){
-			Team.find(afterFindTeams);
-		}
-
-		// Associate keys with teams
-		function afterFindTeams(err, teamData){
-			var teamList = {};
-
-			// Load teams and assing it's key as it's id
-			teamData.forEach(function(team){
-				teamList[team.id] = team;
-			});
-
-			// Includes team object in 'table' data
-			data.forEach(function(group){
-
-				// console.log(group);
-				// Go trough all the table adding key 'team' with team data
-				_.forEach(group.table, function(row){
-					row.team = teamList[row.teamId];
-				});
-			});
-
-			// Includes team object in 'match' data
-			data.forEach(function(group){
-
-				// console.log(group);
-				// Go trough all the table adding key 'team' with team data
-				_.forEach(group.matches, function(row){
-					row.teamA = teamList[row.teamAId];
-					row.teamB = teamList[row.teamBId];
-				});
-			});
-
-			// console.log(data);
-			finishRendering();
-		}
+		findAssociated(id, finishRendering);
 
 		// Render JSON content
-		function finishRendering(){
+		function finishRendering(data){
 			// If none object found with given id return error
 			if(id && !data[0])
 				return next();
@@ -150,7 +62,103 @@ module.exports = {
 		menus: [
 			{name: 'Groups', path: '/groups/manage', order: 1}
 		]
+	},
+
+	_findAssociated: findAssociated
+
+};
+
+/*
+	The objective of this PRIVATE method is to create a full data
+	of the Group, with it's matches, teams, and soccer-tables generated
+*/
+function findAssociated(id, next){
+	var finding = Group.find(options);
+
+	finding.done(function afterFound(err, models) {
+		if(!err)
+			afterFindGroups(models);
+	});
+
+	// Data to be rendered
+	var data = [];
+	// Wait for parallel tasks to complete
+	var completed = 0;
+
+	// After finishing search
+	function afterFindGroups(models){
+		data = models;
+		completed = 0;
+		
+		if(data.length <= 0)
+			return next([]);
+
+		// Load Matches for each Group and associates
+		data.forEach(function(group){
+
+			Match.find()
+				.where({'groupId': group.id})
+				.sort('day ASC')
+				.sort('hour ASC')
+				.sort('id')
+				.then(function(matches){
+				// Associate matches with groups
+				group.matches = matches;
+
+				// Compute scoring table for each group
+				// (must be computed AFTER associating matches with it)
+				group.table = group.table();
+
+				// Callback
+				loadedModel();
+			});
+		});
 	}
 
-	
-};
+	// Function needed to wait for all queries to finish
+	function loadedModel(){
+		completed++;
+
+		if(completed >= data.length)
+			associateTeams();
+	}
+
+	// Load Teams
+	function associateTeams(){
+		Team.find(afterFindTeams);
+	}
+
+	// Associate keys with teams
+	function afterFindTeams(err, teamData){
+		var teamList = {};
+
+		// Load teams and assing it's key as it's id
+		teamData.forEach(function(team){
+			teamList[team.id] = team;
+		});
+
+		// Includes team object in 'table' data
+		data.forEach(function(group){
+
+			// console.log(group);
+			// Go trough all the table adding key 'team' with team data
+			_.forEach(group.table, function(row){
+				row.team = teamList[row.teamId];
+			});
+		});
+
+		// Includes team object in 'match' data
+		data.forEach(function(group){
+
+			// console.log(group);
+			// Go trough all the table adding key 'team' with team data
+			_.forEach(group.matches, function(row){
+				row.teamA = teamList[row.teamAId];
+				row.teamB = teamList[row.teamBId];
+			});
+		});
+
+		// console.log(data);
+		next(data);
+	}
+}
